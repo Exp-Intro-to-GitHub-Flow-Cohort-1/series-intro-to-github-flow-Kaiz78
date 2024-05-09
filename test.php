d6039bead46665981365052a2083540092308b6d
Kaiz78 <luffy4454@gmail.com>
2024-05-09 21:40:10 +0200

    ajout d'un dossier

--- test.php
+++ test.php
@@ -4,3 +4,15 @@
 echo "hello world";
 
 ?>
+
+
+
+script {
+  def deletedFiles = sh(script: 'git diff --diff-filter=D --name-only HEAD^ HEAD', returnStdout: true).trim().split('\n')
+  if (deletedFiles.size() == 0) {
+    sh "rsync -av --delete " + deletedFiles.join(' ') + " user@serveur_distant:destination/
+    return
+  }else{
+    sh "rsync -r " user@serveur_distant:destination/"
+  }
+}
